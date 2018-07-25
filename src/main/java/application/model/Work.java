df2f71812d51865a296f70cf8d7c4a1784c37a46
Death111 <5772511+Death111@users.noreply.github.com>
2018-07-25 14:12:47 +0200

    added getter and setters

--- src/main/java/application/model/Work.java
+++ src/main/java/application/model/Work.java
@@ -18,18 +18,50 @@ public class Work {
    @Column(name = "id", updatable = false, nullable = false)
    private long id;
 
-   public LocalDateTime startTime;
-   public LocalDateTime endTime;
+   private LocalDateTime startTime;
+   private LocalDateTime endTime;
 
    @OneToOne
-   public Project project;
-   public String notes;
+   private final Project project;
+   private String notes;
 
    public Work(final LocalDateTime startTime, final LocalDateTime endTime, final Project project, final String notes) {
       super();
       this.startTime = startTime;
       this.endTime = endTime;
       this.project = project;
+      this.notes = notes;
+   }
+
+   public long getId() {
+      return id;
+   }
+
+   public LocalDateTime getStartTime() {
+      return startTime;
+   }
+
+   public void setStartTime(final LocalDateTime startTime) {
+      this.startTime = startTime;
+   }
+
+   public LocalDateTime getEndTime() {
+      return endTime;
+   }
+
+   public void setEndTime(final LocalDateTime endTime) {
+      this.endTime = endTime;
+   }
+
+   public Project getProject() {
+      return project;
+   }
+
+   public String getNotes() {
+      return notes;
+   }
+
+   public void setNotes(final String notes) {
       this.notes = notes;
    }
 
