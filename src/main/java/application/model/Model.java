d1b7dfe96b303f4dc989b21b886f2da6264cd0f0
Death111 <5772511+Death111@users.noreply.github.com>
2018-07-31 18:10:33 +0200

    added color settings logic and bindings

--- src/main/java/application/model/Model.java
+++ src/main/java/application/model/Model.java
@@ -7,24 +7,29 @@ import javafx.collections.ObservableList;
 import javafx.scene.paint.Color;
 
 public class Model {
+   public static final Color originalHoverBackgroundColor = new Color(54 / 255., 143 / 255., 179 / 255., .1);
+   public static final Color originalHoverFontColor = Color.BLACK;
+   public static final Color originalDefaultBackgroundColor = new Color(54 / 255., 143 / 255., 179 / 255., 0.01);
+   public static final Color originalDefaultFontColor = Color.BLACK;
+
+   public static final Color originalTaskBarFontColor = Color.BLACK;
 
    public final Project DEFAULT_PROJECT = new Project("Idle", Color.ORANGE, false, true);
+   public Project idleProject = DEFAULT_PROJECT;
 
    public ObservableList<Project> availableProjects = FXCollections.observableArrayList();
    public ObservableList<Project> allProjects = FXCollections.observableArrayList();
 
    public ObservableList<Work> pastWorkItems = FXCollections.observableArrayList();
-
    public ObjectProperty<Work> activeWorkItem = new SimpleObjectProperty<>();
 
    public long neededWorkSeconds = 60 * 60 * 8; // 8 hours
 
-   public Project idleProject = DEFAULT_PROJECT;
-   public Color taskBarColor = Color.BLACK;
+   public ObjectProperty<Color> taskBarColor = new SimpleObjectProperty<>(originalTaskBarFontColor);
 
-   private final Color hoverBackgroundColor = new Color(54 / 255., 143 / 255., 179 / 255., .1);
-   private final Color hoverFontColor = Color.BLACK;
-   private final Color defaultBackgroundColor = new Color(54 / 255., 143 / 255., 179 / 255., 0.01);
-   private final Color defaultFontColor = Color.BLACK;
+   public ObjectProperty<Color> hoverBackgroundColor = new SimpleObjectProperty<>(originalHoverBackgroundColor);
+   public ObjectProperty<Color> hoverFontColor = new SimpleObjectProperty<>(originalHoverFontColor);
+   public ObjectProperty<Color> defaultBackgroundColor = new SimpleObjectProperty<>(originalDefaultBackgroundColor);
+   public ObjectProperty<Color> defaultFontColor = new SimpleObjectProperty<>(originalDefaultFontColor);
 
 }
