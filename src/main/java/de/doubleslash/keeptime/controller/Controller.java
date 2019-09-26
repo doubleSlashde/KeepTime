e58eef297e40544d2b2f5a4be8b150ccfd6b7719
Martin Plieske <martin.plieske@doubleslash.de>
2019-09-26 17:23:16 +0200

    removed unused method getCurrentWorkSeconds

--- src/main/java/de/doubleslash/keeptime/controller/Controller.java
+++ src/main/java/de/doubleslash/keeptime/controller/Controller.java
@@ -289,9 +289,4 @@ public class Controller {
 
       return seconds;
    }
-
-   public long getCurrentWorkSeconds() {
-      return Duration.between(model.activeWorkItem.get().getStartTime(), model.activeWorkItem.get().getEndTime())
-            .getSeconds();
-   }
 }
