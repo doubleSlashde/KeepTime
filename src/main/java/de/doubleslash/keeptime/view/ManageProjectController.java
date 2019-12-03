49bc6ca338e838d4dcefc59f8df33c4d312f2d64
Martin Plieske <martin.plieske@doubleslash.de>
2019-12-03 17:50:57 +0100

    extracted literals to constants

--- src/main/java/de/doubleslash/keeptime/view/ManageProjectController.java
+++ src/main/java/de/doubleslash/keeptime/view/ManageProjectController.java
@@ -16,6 +16,8 @@ import javafx.scene.paint.Color;
 
 public class ManageProjectController {
 
+   private static final int DEFAULT_SPINNER_VALUE = 0;
+
    private static final Logger LOG = LoggerFactory.getLogger(ManageProjectController.class);
 
    private Model model;
@@ -80,7 +82,7 @@ public class ManageProjectController {
       if (model != null) {
          final int availableProjectAmount = model.getAllProjects().size();
          sortIndexSpinner
-               .setValueFactory(new IntegerSpinnerValueFactory(0, availableProjectAmount, availableProjectAmount));
+               .setValueFactory(new IntegerSpinnerValueFactory(DEFAULT_SPINNER_VALUE, availableProjectAmount, availableProjectAmount));
          sortIndexSpinner.getValueFactory().setValue(model.getAvailableProjects().size());
       }
    }
