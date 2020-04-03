879df983884e308633af389a4600646bd28034ba
Jan Schraff <jan.schraff@doubleslash.de>
2020-04-03 17:49:51 +0200

    changed focus to project dd

--- src/main/java/de/doubleslash/keeptime/view/ManageWorkController.java
+++ src/main/java/de/doubleslash/keeptime/view/ManageWorkController.java
@@ -91,6 +91,7 @@ public class ManageWorkController {
 
       setProjectUpComboBox();
 
+      Platform.runLater(() -> projectComboBox.requestFocus());
    }
 
    private void setUpTimeSpinner(final Spinner<LocalTime> spinner) {
