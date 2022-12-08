f5049c2bd1f82046e8b46a0239e21a3a9ad11663
ddamke <David.damke@doubleslash.de>
2022-12-08 09:17:28 +0100

    add eclipse code formatter plugin

--- src/test/java/de/doubleslash/keeptime/common/RandomColorPickerTest.java
+++ src/test/java/de/doubleslash/keeptime/common/RandomColorPickerTest.java
@@ -1,45 +1,42 @@
 package de.doubleslash.keeptime.common;
 
-
-import de.doubleslash.keeptime.model.Project;
-import javafx.scene.paint.Color;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import javafx.scene.paint.Color;
 
 class RandomColorPickerTest {
 
-    @Test
-    public void shouldGetUniqueColor() {
+   @Test
+   public void shouldGetUniqueColor() {
 
-        // RED, BLUE
-        // assert unique color is BLUE
+      // RED, BLUE
+      // assert unique color is BLUE
 
-        RandomColorPicker.colors=Arrays.asList(Color.RED,Color.BLUE);
+      RandomColorPicker.colors = Arrays.asList(Color.RED, Color.BLUE);
 
-        Assertions.assertNotEquals(Color.RED.toString(),RandomColorPicker.getUniqueColor(List.of(Color.RED),Color.BLUE).toString());
+      Assertions.assertNotEquals(Color.RED.toString(),
+            RandomColorPicker.getUniqueColor(List.of(Color.RED), Color.BLUE).toString());
 
-    }
-    @Test
-    public void shouldReturnBlackWhenAllColorsTakenAlready(){
-        RandomColorPicker.colors= List.of(Color.RED);
+   }
 
-        Assertions.assertEquals(Color.BLACK,RandomColorPicker.getUniqueColor(List.of(Color.RED),Color.BLUE));
-    }
+   @Test
+   public void shouldReturnBlackWhenAllColorsTakenAlready() {
+      RandomColorPicker.colors = List.of(Color.RED);
 
-    @Test
-    public void shouldReturnUniqueColorWhenGiveBackgroundAndProjectColor(){
-        RandomColorPicker.colors=new ArrayList<>(Arrays.asList(Color.RED,Color.BLUE,Color.GREEN,Color.GRAY));
+      Assertions.assertEquals(Color.BLACK, RandomColorPicker.getUniqueColor(List.of(Color.RED), Color.BLUE));
+   }
 
-        Assertions.assertEquals(Color.GRAY,RandomColorPicker.getUniqueColor(Arrays.asList(Color.RED,Color.GREEN),Color.BLUE));
-    }
+   @Test
+   public void shouldReturnUniqueColorWhenGiveBackgroundAndProjectColor() {
+      RandomColorPicker.colors = new ArrayList<>(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.GRAY));
 
-
-
+      Assertions.assertEquals(Color.GRAY,
+            RandomColorPicker.getUniqueColor(Arrays.asList(Color.RED, Color.GREEN), Color.BLUE));
+   }
 
 }
