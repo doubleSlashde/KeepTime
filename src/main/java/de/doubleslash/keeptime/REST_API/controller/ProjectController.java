94399296c86667c0c7adfcca3a2129ed7c156c41
Kilian Mair <kilian.mair@doubleslash.de>
2023-08-07 16:07:04 +0200

    #165: include GetMappig

--- src/main/java/de/doubleslash/keeptime/REST_API/controller/ProjectController.java
+++ src/main/java/de/doubleslash/keeptime/REST_API/controller/ProjectController.java
@@ -3,23 +3,43 @@ package de.doubleslash.keeptime.REST_API.controller;
 
 import de.doubleslash.keeptime.model.Project;
 import de.doubleslash.keeptime.model.repos.ProjectRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
     private ProjectRepository projectRepository;
+
     public ProjectController(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
     }
 
-@GetMapping("")
-    public List<Project>index(){
-return projectRepository.findAll();
+    @GetMapping("")
+    public List<Project> getProjects() {
+        return projectRepository.findAll();
+    }
+
+    @GetMapping("/project")
+    public Project getProjectById(@RequestParam(name = "id") final long id) {
+        final Optional<Project> project = projectRepository.findById(id);
+
+        if (project.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Project with id '" + id + "' not found");
+
+        return project.get();
+    }
+
+    @GetMapping("/byName")
+    public List<Project> getProjectsByName(@RequestParam(name = "name") final String name) {
+        final List<Project> projects = projectRepository.findAll().stream().filter(project -> project.getName().equals(name)).collect(Collectors.toList());
+
+        return projects;
     }
 }
