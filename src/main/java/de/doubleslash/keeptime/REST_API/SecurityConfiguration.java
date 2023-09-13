672bfad9de623071301da0745185fe55ff18fabe
Kilian Mair <kilian.mair@doubleslash.de>
2023-09-13 10:05:38 +0200

    #165: Include REST API Authentication

---
+++ src/main/java/de/doubleslash/keeptime/REST_API/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package de.doubleslash.keeptime.REST_API;
+
+import org.h2.engine.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+   @Autowired
+   private DataSource dataSource;
+
+   @Autowired
+   public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+      auth.jdbcAuthentication().dataSource(dataSource);
+   }
+
+   @Bean
+   public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+      http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeRequests().anyRequest().authenticated().and().httpBasic();
+      return http.build();
+   }
+
+   @Bean
+   public UserDetailsManager users(@Autowired final AuthenticationManagerBuilder auth) throws Exception {
+      final JdbcUserDetailsManager jdbcUserDetailsManager= new JdbcUserDetailsManager(dataSource);
+      jdbcUserDetailsManager.setAuthenticationManager(auth.getOrBuild());
+      return jdbcUserDetailsManager;
+   }
+}
