cd8adbca8c0222f92dc7dec48f11c3bd481806cc
Kilian Mair <kilian.mair@doubleslash.de>
2023-09-14 09:24:35 +0200

    #165: Add UserRepository

---
+++ src/main/java/de/doubleslash/keeptime/model/User.java
@@ -0,0 +1,24 @@
+package de.doubleslash.keeptime.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="Users")
+public class User {
+
+   @Id
+   @Column(name = "USERNAME")
+   private String userName;
+
+   @Column(name = "PASSWORD")
+   private String password;
+   @Column(name = "ENABLED")
+   private boolean enabled;
+
+
+
+
+}
