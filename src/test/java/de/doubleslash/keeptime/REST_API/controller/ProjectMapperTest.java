4480dc8b1ec36a34e880186816fcba7b7a200e23
Kilian Mair <kilian.mair@doubleslash.de>
2023-12-06 09:39:08 +0100

    IPTE-165: Test Refactoring

--- src/test/java/de/doubleslash/keeptime/REST_API/controller/ProjectMapperTest.java
+++ src/test/java/de/doubleslash/keeptime/REST_API/controller/ProjectMapperTest.java
@@ -1,16 +1,16 @@
 package de.doubleslash.keeptime.REST_API.controller;
 
+import de.doubleslash.keeptime.REST_API.DTO.ColorDTO;
 import de.doubleslash.keeptime.REST_API.DTO.ProjectColorDTO;
 import de.doubleslash.keeptime.REST_API.mapper.ProjectMapper;
 import de.doubleslash.keeptime.model.Project;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectMapperTest {
-   ProjectMapper projectMapper= ProjectMapper.INSTANCE;
+   ProjectMapper projectMapper = ProjectMapper.INSTANCE;
 
    @Test
    void projectToProjectDTO() {
@@ -24,8 +24,6 @@ class ProjectMapperTest {
       project.setEnabled(true);
       project.setWork(false);
 
-
-
       //ACT
       final ProjectColorDTO projectColorDTO = projectMapper.projectToProjectDTO(project);
       //Assert
@@ -33,25 +31,22 @@ class ProjectMapperTest {
 
    }
 
-//   @Test
-//   void projectDTOToProject() {
-//      //ARRANGE
-//      ProjectColorDTO project = new ProjectColorDTO();
-//      project.setName("ProjectName");
-//      project.setDescription("ProjectDescription");
-//      project.setColor();
-//      project.setIndex(0);
-//      project.setDefault(true);
-//      project.setEnabled(true);
-//      project.setWork(false);
-//
-////ColorDTO colorDTO;
-//
-//      //ACT
-//      final ProjectColorDTO projectColorDTO = new ProjectColorDTO("ddssdf","Test", colorDTO.getRed(),colorDTO.getBlue(),true,2,true);
-//            Project project1= projectMapper.projectDTOToProject(projectColorDTO);
-//      //Assert
-//      assertEquals(Color.BLUE.getBlue(), projectColorDTO.getColor().getBlue());
-//
-//   }
+   @Test
+   void projectDTOToProject() {
+      // ARRANGE
+      ColorDTO colorDTO = new ColorDTO();
+      colorDTO.setRed(1.0);
+      colorDTO.setGreen(0.0);
+      colorDTO.setBlue(0.0);
+
+      ProjectColorDTO project = new ProjectColorDTO(1, "ProjectName", "ProjectDescription", colorDTO, false, 0, true);
+
+      // ACT
+      final Project project1 = projectMapper.projectDTOToProject(project);
+
+      // ASSERT
+      assertEquals(colorDTO.getBlue(), project1.getColor().getBlue());
+      assertEquals("ProjectName", project1.getName());
+      assertEquals("ProjectDescription", project1.getDescription());
+   }
 }
